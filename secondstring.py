40c57ba295ded3c1f931dbd8f6dcbbfa675656a8
Cdoyle22 <98051052+Cdoyle22@users.noreply.github.com>
2022-04-08 19:48:48 +0100

    Last Checks - 8th April

--- secondstring.py
+++ secondstring.py
@@ -1,7 +1,13 @@
 # Create a program that asks a user to input a string and outputs every second letter in reverse order.
 #Author:Ciara Doyle
 
-word = str(input("Enter a word:"))
-listofletters = list(word)[::-2]
+word = str(input("Please enter a sentence:"))
+# this asks for a string input which will be called word
 
-print (listofletters)
+listofletters = list(word)[::-2]
+# this uses string slicing to fetch all characters in a string which have an index difference equal to the value supplied at the right starting from the value at the left.
+#Ref: https://codippa.com/how-to-print-characters-at-even-position-in-string-in-python/
+# including - in front of the number ensures it is in reverse
+
+print (*listofletters)
+#This prints every second letter in order from reverse and the asterisk operator is used to unpack an iterable into the argument list of a given function.
