f04baff161fc03837111c72fb2bdd89e632e9b91
Cdoyle22 <98051052+Cdoyle22@users.noreply.github.com>
2022-04-08 20:13:36 +0100

    Last Checks - 8th April

--- collatz.py
+++ collatz.py
@@ -6,17 +6,18 @@
 
 def collatz(number):
 
-    if number % 2 ==0:
+    if number % 2 ==0: # if the number is even
         print(number//2)
-        return number //2
+        return number //2 # then divide it by 2
 
-    elif number % 2 ==1:
-        result = 3* number +1
+    elif number % 2 ==1: # but if it is odd 
+        result = 3* number +1  #multiply it by 3 and add 1  
         print(result)
         return result
-    
-n = input("Enter a number:")
-while n !=1:
+ #ref: https://automatetheboringstuff.com/chapter3/
+ 
+n = input("Please enter a positive integer:") # this is input for the positive integer number
+while n !=1: #when n is not equal to one the while loop will continue. this is the break for when it equals 1
     n = collatz(int(n))
 
 
