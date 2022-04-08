b1cc2580a1fb637d718c75fe1de79bcb0b00c8ee
Cdoyle22 <98051052+Cdoyle22@users.noreply.github.com>
2022-04-08 19:26:05 +0100

    Last Checks - 8th April

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pands-problem-sheet
+
+Weekly Tasks - Programming and Scripting 2022
+
+Course: HDIP in Computing and Data Analytics 
+College: GMIT/ ATU
+Lecturer: Andrew Beatty, Software Architect/Developer
+
+This repository holds code written in response to each weekly task (py.files) assigned as part of the Programming and Scripting Module of this course. Each file contains a description of the problem and how the solution was developed.
+
+https://www.freecodecamp.org/news/how-to-write-a-good-readme-file/
--- bmi.py
+++ bmi.py
@@ -2,8 +2,17 @@
 #Author: Ciara Doyle
 
 weight= float(input('Enter weight (kg):'))
+# this asks for input which will be the weight used in kg
 height= float(input('Enter height (cm):'))
+# this asks for input which will be the height used in cm
 
 BMI= weight / ((height/100) ** 2)
-print(BMI)
+# this calculates the BMI based on the standard calculation of BMI = kg/m2 
+#(This also includes changing cm to m2)
+# ref: https://www.diabetes.ca/managing-my-diabetes/tools---resources/body-mass-index-(bmi)-calculator#:~:text=Body%20Mass%20Index%20is%20a,most%20adults%2018%2D65%20years.
 
+BMIx= round(BMI, 2)
+# this will limit the numbers after the decimal point to 2
+
+print ('The BMI is (kg/m2)', BMIx)
+# This will print out the answer
